0f1bd2c4491e07faa011418bbc97b667537e1042
joshua <jrinard@hotmail.com>
2017-03-21 17:07:51 -0700

    Delete item functionality

--- app/assets/javascripts/components/_all_items.js.jsx
+++ app/assets/javascripts/components/_all_items.js.jsx
@@ -1,11 +1,16 @@
 
 var AllItems = React.createClass({
+  handleDelete(id) {
+    this.props.handleDelete(id);
+  },
+
     render() {
         var items= this.props.items.map((item) => {
             return (
                 <div key={item.id}>
                     <h3>{item.name}</h3>
                     <p>{item.description}</p>
+                    <button onClick={this.handleDelete.bind(this, item.id)}>Delete</button>
                 </div>
             )
         });
@@ -18,3 +23,5 @@ var AllItems = React.createClass({
     }
 });
  //defines all items  // returns all items
+
+//The bind() method will bind the id of the item to this, causing the id to send as an argument.
--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -12,13 +12,31 @@ var Body = React.createClass({
         this.setState({ items: newState })
     },
 
+      handleDelete(id) {
+        $.ajax({
+            url: `/api/v1/items/${id}`,
+            type: 'DELETE',
+            success:() => {
+                this.removeItemClient(id); //removing the item from the array of items upon successful deletion from the database.
+            }
+        });
+    },
+
+    removeItemClient(id) {
+      var newItems = this.state.items.filter((item)=> {
+        return item.id != id;
+      });
+      this.setState({ items: newItems });
+    },
+
     render() {
         return (
             <div>
                 <NewItem handleSubmit={this.handleSubmit}/>
-                <AllItems items={this.state.items} />
+                <AllItems items={this.state.items} handleDelete={this.handleDelete}/>
             </div>
         )
     }
 });
+//props is used to pass the reference of the function in the parent component to the child component
 // sends down the items from body to allitems
